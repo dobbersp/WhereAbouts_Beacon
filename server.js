f8c344ef5a55526add6960fe9ea0d0c0725e08af
dobber <dobbersp@gmail.com>
2020-08-18 20:24:07 -0700

    Updated for version 0.0.2 with new messaging protocol, including markers, routes, and colors.

--- server.js
+++ server.js
@@ -1,27 +1,52 @@
 const path = require('path');
-const { checkServerIdentity } = require("tls");
-
 const https = require("https");
 const io = require("socket.io");
 const fs = require("fs");
 
-var domain = "dobbergps";
-var certDir = "C:\\Certbot\\live\\"+domain+"\\";
+var certDir = "C:\\Certbot\\live\\dobbergps\\";
+var dataDir = ".\\data\\";
+
+var markers = loadDataFileContents("markers.txt");
+var routes = loadDataFileContents("routes.txt");
 
 var options = {
   key: fs.readFileSync(certDir+"privkey.pem"),
   cert: fs.readFileSync(certDir+"fullchain.pem")
-  };
+};
 
 var https_server = https.createServer(options)
 socket_listener = io.listen(https_server);
 https_server.listen(3000);
 
-socket_listener.sockets.on("connection", function(socket) {
-  console.log("user connected: " + socket.request.connection.remoteAddress);
+socket_listener.sockets.on("connection", function(client) {
+  console.log("user connected: " + client.request.connection.remoteAddress);
+  //send current state only to the client connecting (not broadcast to all connected clients.)
+  if(markers != -1)
+    client.emit("markers",markers);
+  if(routes != -1)
+    client.emit("routes",routes);
 });
 
-var dataDir = ".\\data\\";
+function loadDataFileContents(filename)
+{
+  if(!fs.existsSync(dataDir+"\\"+filename))
+  {
+    console.log(filename+ " does not exist.");
+    return -1;
+  }
+  
+  var fileData = fs.readFileSync(dataDir+"\\"+filename);
+  try {
+    var jsonData = JSON.parse(fileData);  
+  }
+  catch (e) 
+  {
+    console.error(e);
+    return -1;
+  }
+  return jsonData;
+}
+
 fs.watch(dataDir,"utf8",handleDataChange);
 
 function handleDataChange(event, trigger)
@@ -31,24 +56,17 @@ function handleDataChange(event, trigger)
     console.log("Skipped file event: " + event + " : " + trigger);
     return;
   }
-  console.log(event+" : "+trigger);
-  if(!fs.existsSync(dataDir+"\\"+trigger))
+  var fileName = path.basename(trigger);
+  var messageType = path.basename(trigger,path.extname(trigger)); //message type is just the filename with no file extension.
+  data = loadDataFileContents(fileName);
+  if(data != -1)
   {
-    console.log("Skipped file event: " + event + " : " + trigger);
-    console.log(dataDir+"\\"+trigger + " does not exist.");
-    return;
-  }
-  var fileData = fs.readFileSync(dataDir+"\\"+trigger);
-  try {
-    var jsonData = JSON.parse(fileData);  
-  }
-  catch (e) 
-  {
-    console.error(e);
-    return;
-  }
-  
-  var fileName = path.basename(trigger,".txt");
-  jsonData.key = fileName;
-  socket_listener.sockets.emit('server_message', jsonData);
+    if(messageType == "markers")
+      markers = data;
+    else if(messageType == "routes")
+      routes = data;
+    else
+      return; //bad filename, don't send a message.
+    socket_listener.sockets.emit(messageType, data); //broadcast update to all clients.
+  }   
 }
